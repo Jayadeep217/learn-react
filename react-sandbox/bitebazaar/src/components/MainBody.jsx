4f3e9138e1c360e9d91b700ac6d20b7dff2bf334
jayadeep217 <gaddamjayadeep@gmail.com>
2025-03-12 16:27:28 +0530

    useState

--- react-sandbox/bitebazaar/src/components/MainBody.jsx
+++ react-sandbox/bitebazaar/src/components/MainBody.jsx
@@ -1,14 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const MainBody = () => {
+  const [restaurantsList, setRestaurantsList] = useState(resList);
+
   return (
     <div className="main">
       <div className="search">
-        <button className="btn-filter">Top rated restaurants</button>
+        <button
+          className="btn-filter"
+          onClick={() => {
+            const filteredRestaurantsList = restaurantsList.filter(
+              (restaurant) => restaurant.data.avgRating > 4.2
+            );
+            setRestaurantsList(filteredRestaurantsList);
+          }}
+        >
+          Top rated restaurants
+        </button>
       </div>
       <div className="restaurant-container">
-        {resList.map((restaurant) => (
+        {restaurantsList.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
